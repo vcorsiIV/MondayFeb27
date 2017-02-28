d8b83596aa5b76110019e14776ade3a42d7a7ac9
vcorsiIV <S01160644@acad.tri-c.edu>
2017-02-27 19:28:19 -0500

    Upload Set2

---
+++ FixedDebugThree2/FixedDebugThree2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FixedDebugThree2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            //Add .Text to covert string to the label, and change goodbye to hello
+            outputLabel.Text = "Hello, " + textBox1.Text + "!";
+        }
+
+        private void goodbyeButton_Click(object sender, EventArgs e)
+        {
+            //Add .Text to covert string to the label
+            outputLabel.Text = "Goodbye, " + textBox1.Text + "!";
+        }
+    }
+}
---
+++ FixedDebugThree4/FixedDebugThree4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FixedDebugThree4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int birth;
+            int year;
+            //add int age
+            int age;
+            birth = Convert.ToInt32(textBox1.Text);
+            year = Convert.ToInt32(textBox2.Text);
+            //add a semicolon, and change the + to -
+            age = year - birth;
+            outputLabel.Text = String.Format("On your birthday this year, \nyou were or will be " +
+                "{0} years old", age);
+        }
+    }
+}
